0fb9c1ea3f515121faeb8114d4dbe4338877e718
Gaetan <gaetan.peltier@protonmail.com>
2019-09-17 13:47:41 +0200

    Create shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage/Homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route exact path='/' component={Homepage} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
