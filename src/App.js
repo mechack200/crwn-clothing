6903a83e4083912a6cbf453c03a70e4c35d9efd4
Gaetan <gaetan.peltier@protonmail.com>
2019-09-22 21:07:50 +0200

    add redux, implemented userReducer and userAction to header and App component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React from 'react';
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Switch, Route } from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  state = {
-    currentUser: '',
-  }
-  
+
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (!userAuth) {
-        return this.setState({
-          currentUser: userAuth
-        })
+        return setCurrentUser(userAuth)
       }
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot(snapshop => {
-          this.setState({
-            currentUser: snapshop.id,
+          setCurrentUser({
+            id: snapshop.id,
             ...snapshop.data()
           })
         })
@@ -41,18 +38,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Header currentUser={this.state.currentUser}/>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/shop" component={ShopPage} />
-            <Route exact path="/signin" component={SignInAndSignUpPage} />
-          </Switch>
-        </BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
       </div>
     );
   }
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App)
--- src/components/sign-in/sign-in-component.jsx
+++ src/components/sign-in/sign-in-component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss'
 import FormInput from '../FormInput/Forminput.component'
 import CustomButton from '../custom-button/CustomButton.component'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
   state = {
@@ -13,10 +13,19 @@ class SignIn extends React.Component {
     password: '',
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault()
-    this.setState({ email: '', password: '' })
+    const {email, password} = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({
+        email: '',
+        password: '',
+      })
+    } catch (error) {
   }
+}
 
   handleChange = (event) => {
     const { value, name } = event.target
